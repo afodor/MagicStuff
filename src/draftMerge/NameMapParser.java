8761953f693fd2a50b586ab5548837c474821836
afodor <afodor@CCI7NR4PN2WWS.dyn.uncc.edu>
2019-11-26 19:59:55 -0500

    linked

--- src/draftMerge/NameMapParser.java
+++ src/draftMerge/NameMapParser.java
@@ -11,21 +11,45 @@ public class NameMapParser
 {
 	public static void main(String[] args) throws Exception
 	{
-		HashMap<Integer, String> map = getMap();
-		
-		BufferedWriter writer = new BufferedWriter(new FileWriter("c:\\temp\\test.txt"));
-		
-		writer.write( "id\tname\n" );
-		
-		for(Integer i : map.keySet())
-			writer.write(i + "\t" + map.get(i) + "\n");
-		
-		writer.flush(); writer.close();
+		HashMap<String, Integer> idMap = getIDMap();
+		HashMap<Integer, Holder> holderMap = getHolderMap(idMap);
 	}
 	
-	public static HashMap<Integer, String> getMap() throws Exception
+	
+	private static class Holder
 	{
-		HashMap<Integer, String> map = new HashMap<>();
+		String cardName;
+		double avgSeenAt;
+		double avgTakenAt;
+	}
+	
+	public static HashMap<Integer, Holder> getHolderMap(HashMap<String, Integer> idMap ) throws Exception
+	{
+		HashMap<Integer, Holder> map = new HashMap<>();
+		
+		BufferedReader reader = new BufferedReader(new FileReader(
+				"C:\\mtga_Arena_tool\\ranks.txt"));
+		
+		reader.readLine();
+		
+		for(String s = reader.readLine(); s != null; s= reader.readLine())
+		{
+			String[] splits = s.split("\t");
+			
+			String key = splits[0].replaceAll("\"", "");
+			Integer i = idMap.get(key);
+			
+			if( i == null)
+				System.out.println("Could not find " + splits[0]);
+		}
+		
+		
+		return map;
+	}
+	
+	public static HashMap<String, Integer> getIDMap() throws Exception
+	{
+		HashMap<String, Integer> map = new HashMap<>();
 		
 		BufferedReader reader =new BufferedReader(new FileReader(new File(
 				"C:\\mtga_Arena_tool\\MTG-Arena-Tool-master\\src\\resources\\database.json")));
@@ -41,7 +65,7 @@ public class NameMapParser
 			Integer key = Integer.parseInt(splits[x].substring(1,splits[x].indexOf(",") ));
 			int endPos = splits[x].indexOf( "\"set\"");
 			String val =splits[x].substring(splits[x].indexOf(",")+8, endPos-2).replaceAll("\"", ""); 
-			map.put(key,val);
+			map.put(val,key);
 		}
 		
 		return map;
